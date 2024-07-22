a7845df6083e9c4d74f1fa5620a09c08ed739421
MaryConnolly5 <mvc6293@psu.edu>
2024-07-22 13:35:24 -0400

    updates

--- index.php
+++ index.php
@@ -4,8 +4,60 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IEP Industrial Beta</title>
+    
 </head>
 <body>
     <p>Hello World!</p>
+
+    <header class="banner">
+        <img src="logo.png" alt="IEP Logo" class="logo">
+        <nav class="navigation">
+            <button class="nav-bton">Home</button>
+            <div class="dropdown">
+                <button class="nav-btn dropdwon-btn">Industrial Products</button>
+                <div class="dropdown-content">
+                    <a href="#">Fireproofing Tape</a>
+                    <a href="#">Refactory Insulation/Thermal Insulation</a>
+                    <a href="#">Mortar</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="nav-btn dropdown-btn">Custom Fabrication</button>
+                <div class="dropdwon-content">
+                    <a href="#">Fire Containment Bags</a>
+                    <a href="#">Fire Resistent Pouches</a>
+                    <a href="#">Fire Protective Products</a>
+                    <a href="#">High Temperature Textiles</a>
+                    <a href="#">Tadpole Gaskets</a>
+                    <a href="#">Fire Rescure Products</a>
+                </div>
+            </div>
+
+            <button class="nav-btn">Contact Us</button>
+        </nav>
+        </header>
+
+        <main>
+            <section class="here-section"></section>
+            <h1>Welcome to IEP</h1>
+            <p>This is a test for the site</p>
+        </section>
+
+        <section class="content-section">
+            <h2>About IEP</h2>
+            <p>This is a bit about IEP as a company and what we do.</p>
+        </section>
+        
+        <section class="content-section">
+            <h2>Contact Us</h2>
+            <p>Contact information for IEP can be found here</p>
+        </section>
+        </main>
+
+        <footer>
+            <p>&copy; 2024 Industrial Energy Products Inc. All rights reserved.</p>
+        </footer>
+
+        <script src="script.js"></script>
 </body>
 </html>
